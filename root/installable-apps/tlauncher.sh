f02023653861ceb0670ac144bd0a1169fa64eb8a
chompypotato <fletcher.hoch@outlook.com>
2025-03-30 17:59:24 -0400

    Create tlauncher.sh

---
+++ root/installable-apps/tlauncher.sh
@@ -0,0 +1,30 @@
+echo "**** Fixing broken dependencies ****"
+echo "**** why are you installing this the mc launcher is on here already but ok ****"
+echo "**** here we go ****"
+apt update
+apt --fix-broken install -y
+
+echo "**** Installing OpenJDK 17 ****"
+apt install -y openjdk-17-jdk wget unzip
+
+echo "**** Downloading TLauncher ****"
+wget -O /tmp/TLauncher.jar https://repo.tlauncher.org/update/lch/starter-core-1.11-v10.jar
+mkdir -p /opt/tlauncher
+mv /tmp/TLauncher.jar /opt/tlauncher/
+
+cat > /usr/share/applications/tlauncher.desktop <<EOL
+[Desktop Entry]
+Name=TLauncher
+Comment=Minecraft launcher
+Exec=java -jar /opt/tlauncher/TLauncher.jar
+Icon=/opt/tlauncher/TLauncher_icon.png
+Terminal=false
+Type=Application
+Categories=Game;
+EOL
+
+wget -O /opt/tlauncher/TLauncher_icon.png https://cdn.icon-icons.com/icons2/2699/PNG/512/minecraft_logo_icon_168974.png
+
+chmod +x /usr/share/applications/tlauncher.desktop
+
+echo "**** TLauncher installation completed ****"
